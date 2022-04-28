670eef61a19d575b2f5f4085877ef53fb6527da7
Julian Pineda <918851+julianpinedayyz@users.noreply.github.com>
2022-04-28 09:59:07 -0400

    creating column component

---
+++ src/Column.svelte
@@ -0,0 +1,14 @@
+<script>
+   import { fly, scale } from 'svelte/transition';
+  const remove = (e) => e.target.parentElement.remove();
+  export let content;
+  let claps = 0;
+  const clap = (e) => claps++;
+</script>
+
+<div class="item"  in:fly="{{ y: 200, duration: 300 }}">
+  <span class="content" contenteditable="">{content}</span>
+
+  <button class="remove" on:click={remove}>x</button>
+  <span class="claps" on:click="{clap}">👏x{claps}</span>
+</div>
